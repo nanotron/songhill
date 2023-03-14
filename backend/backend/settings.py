bb27a6bc196d3a1803a4f7bc51b575a72502374d
Ernest Millan <millan@gmail.com>
2023-03-14 13:42:34 -0700

    another token fix attempt for post

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,7 +182,6 @@ CSRF_TRUSTED_ORIGINS = [
 # ]
 # CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
   'http://0.0.0.0',
   'http://localhost',
