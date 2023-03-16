ad7f6b529e9068e34f23f40247cafebe091ad773
Ernest Millan <millan@gmail.com>
2023-03-16 08:04:37 -0700

    csrf test fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -178,8 +178,8 @@ CSRF_TRUSTED_ORIGINS = [
 #     "content-type",
 #     "X-CSRFToken"
 # ]
-# CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
   'http://0.0.0.0',
   'http://localhost',
